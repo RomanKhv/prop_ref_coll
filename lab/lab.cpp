a02756b5bf3bd9707b74090ce115edb2470acbb5
Roman Khvostikov <roman_khvostikov@mentor.com>
2019-05-16 18:16:00 +0300

    added "lab" project

---
+++ lab/lab.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include <boost/test/unit_test.hpp>
+
+boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+	return nullptr;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( lab )
+{
+	BOOST_CHECK( false );
+}
---
+++ lab/pch.h
@@ -0,0 +1,6 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "boost/test/test_tools.hpp"
+
+#endif //PCH_H
