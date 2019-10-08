323546c7bc31aa46e83903dbe504676640444a41
Roman Khvostikov <roman_khvostikov@mentor.com>
2019-10-08 18:04:03 +0300

    ptree basics

--- lab/mpl.h
+++ lab/mpl.h
@@ -1,21 +1,23 @@
 #pragma once
 
+#include <type_traits>
+
 namespace mpl
 {
 //////////////////////////////////////////////////////////////////////////
 
-namespace detail
-{
-template <class...> struct void_type { using type = void; };
-}
-
-	template <class... T>
-	using void_t = typename detail::void_type<T...>::type;
+// namespace detail
+// {
+// template <class...> struct void_type { using type = void; };
+// }
+// 
+// 	template <class... T>
+// 	using void_t = typename detail::void_type<T...>::type;
 
 //////////////////////////////////////////////////////////////////////////
 
 	template <class T>
-	using is_range = mpl::void_t
+	using is_range = /*mpl*/std::void_t
 		< decltype(std::declval<T>().begin())
 		, decltype(std::declval<T>().end())
 		, decltype(std::declval<T>().size()) >;
--- lab/prc_v1.cpp
+++ lab/prc_v1.cpp
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 #include "mpl.h"
 
@@ -23,12 +24,16 @@
 */
 
 // custom types
-enum enum_t { enum1, enum2 };
+enum enum_t { enum1, enum2, enum3, enum4, enum5 };
+
+static const std::vector<int> ref_vec_of_ints = { 1, 2, 3 };
+static const std::vector<enum_t> ref_vec_of_enums = { enum1, enum2, enum3 };
+static const std::vector<std::vector<int>> ref_vec_of_vec_of_ints = { {1,2,3}, {4,5} };
 
 //////////////////////////////////////////////////////////////////////////
 
 static std::string _output;
-static boost::property_tree::ptree _ptree;
+static boost::property_tree::wptree _ptree;
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -100,6 +105,8 @@ namespace prc
 	};
 }
 
+//////////////////////////////////////////////////////////////////////////
+
 BOOST_AUTO_TEST_CASE( test_copy_equal )
 {
 	{
@@ -133,3 +140,157 @@ BOOST_AUTO_TEST_CASE( test_copy_equal )
 		BOOST_CHECK( prc::op_equal( dst, src ) );
 	}
 }
+
+//////////////////////////////////////////////////////////////////////////
+
+namespace prc
+{
+	template <class S, class T, class = void>
+	struct io_gate
+	{
+		static void save( S& stream, const wchar_t* tag, const T& value )
+		{
+			stream << value;
+		}
+		static void load( const S& stream, const wchar_t* tag, T& value )
+		{
+			stream >> value;
+		}
+	};
+
+	template <class S, class T>
+	void op_save( S& stream, const wchar_t* tag, const T& value )
+	{
+		io_gate<S,T>::save( stream, tag, value );
+	}
+
+	template <class S, class T>
+	void op_load( const S& stream, const wchar_t* tag, T& value )
+	{
+		io_gate<S,T>::load( stream, tag, value );
+	}
+}
+
+// ptree
+namespace prc
+{
+	template <class T, class = void>
+	struct io_ptree_single
+	{
+		static void save( boost::property_tree::wptree& node, const T& value )
+		{
+			node.put_value( value );
+		}
+		static void load( const boost::property_tree::wptree& node, T& value )
+		{
+			if ( boost::optional<T> node_val = node.get_value_optional<T>() )
+				value = *node_val;
+		}
+	};
+
+	template <class T>
+	struct io_ptree_single< T, typename std::enable_if_t<std::is_enum_v<T>> >
+	{
+		static void save( boost::property_tree::wptree& node, const T& value )
+		{
+			node.put_value( (long&)value );
+		}
+		static void load( const boost::property_tree::wptree& node, T& value )
+		{
+			if ( boost::optional<long> node_val = node.get_value_optional<long>() )
+				value = (T)*node_val;
+		}
+	};
+
+	template <class T>
+	struct io_gate< boost::property_tree::wptree, T >
+	{
+		static void save( boost::property_tree::wptree& stream, const wchar_t* tag, const T& value )
+		{
+			boost::property_tree::wptree node;
+			io_ptree_single<T>::save( node, value );
+			stream.put_child( tag, node );
+		}
+		static void load( const boost::property_tree::wptree& stream, const wchar_t* tag, T& value )
+		{
+			boost::optional<const boost::property_tree::wptree&> node = stream.get_child_optional( tag );
+			if ( node )
+			{
+				io_ptree_single<T>::load( *node, value );
+			}
+		}
+	};
+
+	template <class T>
+	struct io_gate< boost::property_tree::wptree, std::vector<T> >
+	{
+		using CONTAINER = std::vector<T>;
+		static void save( boost::property_tree::wptree& stream, const wchar_t* tag, const CONTAINER& container )
+		{
+			boost::property_tree::wptree array_node;
+			for ( const auto& value : container )
+			{
+				boost::property_tree::wptree prop_node;
+				//io_gate<boost::property_tree::wptree,T>::save( prop_node, , value );
+				io_ptree_single<T>::save( prop_node, value );
+				array_node.add_child( L"item", prop_node );
+			}
+			stream.put_child( tag, array_node );
+		}
+		static void load( const boost::property_tree::wptree& stream, const wchar_t* tag, CONTAINER& container )
+		{
+			boost::optional<const boost::property_tree::wptree&> array_node = stream.get_child_optional( tag );
+			if ( array_node )
+			{
+				container.clear();
+
+				for ( const boost::property_tree::wptree::value_type& item_node : *array_node )
+				{
+					_ASSERTE( item_node.first == L"item" );
+					T item;
+					io_ptree_single<T>::load( item_node.second, item );
+					container.push_back( item );
+				}
+			}
+		}
+	};
+}
+
+#define TEST_IO_FILE( T, ref_value, tag, filename ) \
+	{ \
+		const T rhs = (ref_value); \
+		T lhs; \
+		boost::property_tree::wptree pt; \
+		prc::op_save( pt, tag, rhs ); \
+		prc::op_load( pt, tag, lhs ); \
+		BOOST_CHECK( prc::op_equal( lhs, rhs ) ); \
+		if ( filename ) { boost::property_tree::xml_writer_settings<std::wstring> xmlps; xmlps.indent_count = 4; boost::property_tree::write_xml( filename, pt, std::locale(), xmlps ); } \
+	}
+
+#define TEST_IO( T, ref_value, tag ) TEST_IO_FILE( T, ref_value, tag, nullptr )
+
+BOOST_AUTO_TEST_CASE( test_ptree )
+{
+	TEST_IO( int, 273, L"my_int" );
+	TEST_IO( double, 3.14, L"my_double" );
+	TEST_IO( enum_t, enum2, L"my_enum" );
+	TEST_IO( std::wstring, L"sample string", L"my_string" );
+	TEST_IO_FILE( std::vector<int>, ref_vec_of_ints, L"ivec", "vec_of_ints.xml" );
+	TEST_IO( std::vector<enum_t>, ref_vec_of_enums, L"evec" );
+	//TEST_IO( std::vector<std::vector<int>>, ref_vec_of_vec_of_ints, L"ivecvec" );
+// 	{
+// 		std::vector<enum_t> v = { enum1, enum2, enum3 };
+// 		std::vector<int> lhs;
+// 		boost::property_tree::wptree pt;
+// 		prc::op_save( pt, L"arr", rhs );
+// 		prc::op_load( pt, L"arr", lhs );
+// 		BOOST_CHECK( prc::op_equal( lhs, rhs ) );
+// 	}
+// 	{
+// 		const std::string src = "my string";			std::string dst;
+// 		prc::op_copy( dst, src );
+// 		BOOST_CHECK( prc::op_equal( dst, src ) );
+// 	}
+}
+
+//////////////////////////////////////////////////////////////////////////
